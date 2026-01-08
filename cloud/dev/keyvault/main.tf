928fd9af25f07672fbe1657551759ccbee55f0d7
andlogreg <120428961+andlogreg@users.noreply.github.com>
2026-01-08 22:38:09 +0000

    feat(infra,enternal-secrets): add external secrets resources and update keyvault terraform specs

--- cloud/dev/keyvault/main.tf
+++ cloud/dev/keyvault/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 3.0"
+    }
   }
   backend "azurerm" {
     key = "homelab/cloud/dev/keyvault/terraform.tfstate"
@@ -14,17 +18,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "my-rg" {
-  name     = "rg-homelab-dev"
-  location = var.location
-  tags     = var.tags
-}
+provider "azuread" {}
+
+data "azuread_client_config" "current" {}
 
 module "keyvault" {
   source = "../../_modules/keyvault"
 
   keyvault_name       = var.keyvault_name
-  resource_group_name = azurerm_resource_group.my-rg.name
+  resource_group_name = "rg-homelab-dev"
   location            = var.location
   sku_name            = "standard"
 
@@ -44,4 +46,37 @@ output "keyvault_id" {
 
 output "keyvault_uri" {
   value = module.keyvault.vault_uri
+}
+
+resource "azuread_application" "eso" {
+  display_name = "sp-homelab-dev-eso"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "eso" {
+  client_id = azuread_application.eso.client_id
+  owners    = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal_password" "eso" {
+  service_principal_id = azuread_service_principal.eso.id
+}
+
+resource "azurerm_role_assignment" "eso_kv_secrets" {
+  scope                = module.keyvault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azuread_service_principal.eso.object_id
+}
+
+output "eso_sp_client_id" {
+  value = azuread_application.eso.client_id
+}
+
+output "eso_sp_client_secret" {
+  value     = azuread_service_principal_password.eso.value
+  sensitive = true
+}
+
+output "azure_tenant_id" {
+  value = data.azuread_client_config.current.tenant_id
 }
---
+++ cloud/dev/resource_group/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+  backend "azurerm" {
+    key = "homelab/cloud/dev/resource_group/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "my-rg" {
+  name     = "rg-homelab-dev"
+  location = var.location
+  tags     = var.tags
+}
