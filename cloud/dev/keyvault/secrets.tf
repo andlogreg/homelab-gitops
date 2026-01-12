fc05377a389bc027f55020aaadec28c0531f7c03
andlogreg <120428961+andlogreg@users.noreply.github.com>
2026-01-12 13:41:23 +0000

    feat(cloud,secrets): add rotation

--- cloud/dev/keyvault/secrets.tf
+++ cloud/dev/keyvault/secrets.tf
@@ -1,21 +1,15 @@
 resource "time_rotating" "secret_rotation" {
-  # NOTE: 1 hour only for testing purposes
-  rotation_hours = 1
+  # NOTE: 5 minutes only for testing purposes
+  rotation_minutes = 5
 }
 
 #### Mealie Secrets ####
 resource "random_password" "mealie_db_credentials_password" {
-  length  = 20
+  length  = 40
   special = true
-  #   keepers = {
-  #     rotation_time = time_rotating.secret_rotation.id
-  #   }
-}
-
-resource "azurerm_key_vault_secret" "mealie_db_credentials_user" {
-  name         = "mealie-db-credentials-user"
-  value        = "mealie"
-  key_vault_id = module.keyvault.id
+  keepers = {
+    rotation_time = time_rotating.secret_rotation.id
+  }
 }
 
 resource "azurerm_key_vault_secret" "mealie_db_credentials_password" {
