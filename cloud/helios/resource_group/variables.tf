978b0f9424caf5c3f629d6e77e05a75c549ceae7
andlogreg <120428961+andlogreg@users.noreply.github.com>
2026-01-14 17:53:34 +0000

    feat(cloud,helios): add terraform for resource group and keyvault

---
+++ cloud/helios/resource_group/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resources."
+  type        = map(string)
+  default = {
+    project     = "homelab"
+    environment = "helios"
+  }
+}
