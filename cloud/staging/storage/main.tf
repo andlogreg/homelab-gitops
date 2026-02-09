40565e7e814614c715f3d3163a79116e39be8fb2
andlogreg <120428961+andlogreg@users.noreply.github.com>
2026-02-09 23:07:12 +0000

    feat(velero,staging): add manifests

--- cloud/staging/storage/main.tf
+++ cloud/staging/storage/main.tf
@@ -31,7 +31,7 @@ module "storage" {
   resource_group_name  = "rg-homelab-staging"
 
   # Common backup targets
-  containers = ["cnpg-backups"]
+  containers = ["cnpg-backups", "velero-backups"]
 
   tags = {
     environment = "staging"
